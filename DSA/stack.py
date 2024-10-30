c26eb07c6b073a349b93e71df62b995a28279a47
raphaelandreeeee <raphaelandreilontoc@gmail.com>
2024-10-30 21:28:26 +0800

    New implementation of Stack without using Python Lists.

--- DSA/stack.py
+++ DSA/stack.py
@@ -5,44 +5,104 @@ Last element inserted into the stack is the first one to be removed.
 """
 
 
-class Stack:
-    def __init__(self) -> None:
-        self.stack = []
+class Node:
+    """
+    Creates a node. A node is a single element in a stack.
+    The next_node attribute points to the next node in the stack.
+    """
 
-    def push(self, data) -> None:
-        """
-        Adds an element to the top of the stack.
-        """
-        self.stack = self.stack + [data]
-
-    def pop(self) -> None:
-        """
-        Removes the top element from the stack. 
-        """
-        if self.stack:
-            del self.stack[-1]
-        else:
-            self.stack = []
+    def __init__(self, data=None) -> None:
+        self.data = data
+        self.next_node = None
 
     def __repr__(self) -> str:
         """
-        Returns a visual representation of the stack.
+        Returns a visual representation of the node.
         """
-        return f"Stack: {self.stack}"
+        return f"Node: {self.data}"
+
+
+class Stack:
+
+    def __init__(self) -> None:
+        self.top = None
+        self.next = None
+        self.size = 0
+
+    def push(self, data) -> None:
+        """
+        Inserts a data on the top of the stack.
+        """
+        node = Node(data)
+        self.size += 1
+
+        if self.top is None:
+            self.top = node
+        else:
+            node.next_node = self.top
+            self.top = node
+
+    def pop(self) -> any:
+        """
+        Removes and returns the top item on the stack.
+        """
+        item = self.top
+        new_top = item.next_node
+        self.top = new_top
+
+        self.size -= 1
+
+        return item.data
+
+    def peek(self) -> any:
+        """
+        Returns the top item on the stack.
+        """
+        return self.top.data
+
+    def is_empty(self) -> bool:
+        """
+        Returns True if the stack is empty. Returns False otherwise.
+        """
+        return self.top is None
+
+    def __repr__(self) -> str:
+        """
+        Returns the visual representation of the stack.
+        """
+        current = self.top
+        container = ""
+
+        while current:
+            container = f"{current.data}, " + container
+            if current.next_node is None:
+                break
+            current = current.next_node
+
+        return f"[{container}]"
+
+    def __len__(self) -> int:
+        """
+        Returns the size of the stack.
+        """
+        return self.size
 
 
 if __name__ == "__main__":
     stack = Stack()
-    stack.push(1)
-    stack.push(2)
-    stack.push(3)
-    print(stack)                    # Output: Stack: [3, 2, 1]
 
-    stack.pop()
-    print(stack)                    # Output: Stack: [2, 1]
+    stack.push(10)
+    print(stack)
 
-    stack.pop()
-    print(stack)                    # Output: Stack: [1]
+    stack.push(20)
+    stack.push(30)
+    stack.push(40)
+    stack.push(50)
+    print(stack)
 
-    stack.pop()
-    print(stack)                    # Output: Stack: []
+    item = stack.pop()
+    print(stack, item)
+
+    print(stack.peek())
+
+    print(stack.__len__())
